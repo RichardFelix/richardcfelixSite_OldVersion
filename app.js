ebf055f5377b4b2a7ae5e2aa841b7974dacc19f7
Richard Felix <rfelixmail@gmail.com>
2015-10-10 00:45:25 -0400

    added jade templating

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 var express = require('express');
-var app = express();
+var app = express()
 var compression = require('compression');
 
 // gzip enabled for faster loading
 app.use(compression());
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
+// add jade as the engine
+app.set('view engine', 'jade');
+// unminify jade html output
+app.locals.pretty = true;
+
 var port = process.env.PORT || 3000;
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/'));
+ // set the home page route
+app.get("/", function (req, res) {
+  res.render("index.jade");
+});
 
-    // set the home page route
-    app.get("/", function (req, res) {
-      res.redirect("/index.html");
-    });
-
-    // redirect if error 404 or any other 
-    app.use(function(req, res){
-      res.redirect("/index.html");
-    });
+// redirect if error 404 or any other 
+app.use(function(req, res){
+    res.render("index.jade");
+});
 
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
