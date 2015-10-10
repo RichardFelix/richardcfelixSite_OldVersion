a8d82c1b102f80102eef9e040ce3a716b0adc6f0
Richard Felix <rfelixmail@gmail.com>
2015-10-10 00:31:17 -0400

    refactored server

--- app.js
+++ app.js
@@ -7,45 +7,19 @@ app.use(compression());
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 3000,
-    bodyParser = require('body-parser'),
-    sendgrid  = require('sendgrid')('rfelix', 'richiesd22');
-
-app.use(bodyParser.urlencoded({ extended: true })); 
+var port = process.env.PORT || 3000;
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/'));
 
-    //submit form function
-    app.post('/form', function(req, res){
-        
-        sendgrid.send({
-          to:       'me@richardcfelix.com',
-          from:     'me@richardcfelix.com',
-          name:     req.body.contact_name,
-          subject:  'Contact Form',
-          html:     '<h1>Website Contact Form</h1>  <b>NAME:</b> ' + req.body.contact_name + '<br/><br/><b>EMAIL:</b> ' +req.body.contact_email + '<br/><br/> <b>PHONE:</b> ' + req.body.contact_phoneno + ' <br/><br/> <b>MESSAGE:</b> ' + req.body.contact_message 
-            
-        }, function(err, json) {
-            
-          if (err) { 
-              return console.error(err); 
-          }else{ 
-              console.log('Success'); 
-              res.redirect('/thank-you.html'); 
-          }
-         }
-        ); 
-    });
-
     // set the home page route
     app.get("/", function (req, res) {
-      res.redirect("/index3.html");
+      res.redirect("/index.html");
     });
 
     // redirect if error 404 or any other 
     app.use(function(req, res){
-      res.redirect("/index3.html");
+      res.redirect("/index.html");
     });
 
 app.listen(port, function() {
