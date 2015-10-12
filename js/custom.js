bbc0ba66c0c0d3e05c300ec69a22805bda3c12d6
Richard Felix <rfelixmail@gmail.com>
2015-10-11 23:21:28 -0400

    logo and padding refactor

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+//(function(){
+//    image = document.getElementsByClassName('img');
+//    image.style.width = 100%
+//        image.addClass
+//}());
+//    
+////window.onload = function () {
+////alert('greta');
+////      var logo = document.getElementById('img');
+////      image.style.width = 100%
+////
+////};
+//    
+//    function codeAddress() {
+//            alert('ok');
+//        }
+//        window.onload = codeAddress;
