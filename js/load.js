c969769fe839bb603ab81ac09c1073b48f03210e
Richard Felix <rfelixmail@gmail.com>
2015-10-12 00:02:20 -0400

    refactor base.jade and added open sans font

---
+++ js/load.js
@@ -0,0 +1,7 @@
+
+      window.onload = function (){
+        if(window.innerWidth < 700 ){
+          image = document.getElementById('img');
+          image.setAttribute('class','col-12')
+        }
+      };
