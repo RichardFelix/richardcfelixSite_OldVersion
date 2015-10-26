0be13e432291ae90d79d53e6d04327893b2b2350
Richard Felix <rfelixmail@gmail.com>
2015-10-25 21:11:04 -0400

    edited pages

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express'),
     app = express(),
     compression = require('compression'),
-    routes = require('./routes');
+    routes = require('./routes')
+    bodyParser = require('body-parser'),
+    sendgrid  = require('sendgrid')('rfelix', 'richiesd22');;
+
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 // gzip enabled for faster loading
 app.use(compression());
@@ -16,6 +20,28 @@ app.use(express.static(__dirname + '/'));
 
 var port = process.env.PORT || 3000;
 
+//submit form function
+    app.post('/form', function(req, res){
+        
+        sendgrid.send({
+          to:       'me@richardcfelix.com',
+          from:     'me@richardcfelix.com',
+          name:     req.body.name,
+          subject:  'Website Contact Form',
+          html:     '<h1>Website Contact Form</h1>  <b>NAME:</b> ' + req.body.name + '<br/><br/><b>EMAIL:</b> ' +req.body.email + '<br/><br/> <b>PHONE:</b> ' + req.body.tel + ' <br/><br/> <b>MESSAGE:</b> ' + req.body.message 
+            
+        }, function(err, json) {
+            
+          if (err) { 
+              return console.error(err); 
+          }else{ 
+              console.log('Success'); 
+              //res.render('../views/partials/thankyou.html'); 
+          }
+         }
+        ); 
+    });
+
  // routes
 app.get('/', routes.index);
 //app.get('/partials/:name', routes.partials);
@@ -24,3 +50,13 @@ app.get('/', routes.index);
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
+
+
+
+
+
+
+
+
+
+    
