d34a88b80e821d85163647c8cb367f2020c971d4
Richard Felix <rfelixmail@gmail.com>
2015-11-02 23:59:22 -0500

    consildated assets into public folder

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,9 +1,9 @@
 window.onload = function(){
     if(window.innerWidth < 700 ){
       document.getElementById('img').setAttribute('class','col-12');
-      document.body.style.backgroundImage = "url('../img/1-mobile.jpg')"; 
+      document.body.style.backgroundImage = "url('../public/img/1-mobile.jpg')"; 
     }else{
-      document.body.style.backgroundImage = "url('../img/1.jpg')"
+      document.body.style.backgroundImage = "url('../public/img/1.jpg')"
     }
     
 //    if(window.location.pathname == '/'){
